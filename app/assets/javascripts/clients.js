a8ce26a6da0b8e07cbcb4951bad91b7e3c6c6475
Jimmy Lindsey <bkjimmie@gmail.com>
2017-08-02 11:08:30 -0400

    add typed.js

---
+++ app/assets/javascripts/clients.js
@@ -0,0 +1,6 @@
+$(function(){
+  $("#typed").typed({
+    strings: ["Government", "Import/Export", "Infrastructure", "Agriculture", "Construction", "Textile", "Startup"],
+    typeSpeed: 100
+  });
+});
